f3d2ad74216d924dd3bc07d6baf8ae9381e166b3
Red Forks <redforks@gmail.com>
2021-02-07 11:28:02 +0800

    add self, arrow php macro, add del key

--- json2c.sh
+++ json2c.sh
@@ -5,3 +5,5 @@ docker run --rm -w /qmk_firmware/ -v "$PWD":/qmk_firmware --user $(id -u):$(id -
 sed -i 's/HYPR(KC_Y)/VIM_YANK/' ./keyboards/bm40hsrgb/keymaps/my/keymap.c
 sed -i 's/HYPR(KC_P)/VIM_PASTE/' ./keyboards/bm40hsrgb/keymaps/my/keymap.c
 sed -i 's/HYPR(KC_T)/PHP_THIS/' ./keyboards/bm40hsrgb/keymaps/my/keymap.c
+sed -i 's/HYPR(KC_A)/PHP_ARROW/' ./keyboards/bm40hsrgb/keymaps/my/keymap.c
+sed -i 's/HYPR(KC_S)/PHP_SELF/' ./keyboards/bm40hsrgb/keymaps/my/keymap.c
