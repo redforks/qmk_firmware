7d2a5d207b7c3a0651111a63a67f9bfc199a5569
Red Forks <redforks@gmail.com>
2021-02-21 09:30:50 +0800

    Remove hack of sed, qmk config supports any key codes

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
+cat ./keyboards/bm40hsrgb/keymaps/my/keymap.header > ./keyboards/bm40hsrgb/keymaps/my/keymap.c
+docker run --rm -w /qmk_firmware/ -v "$PWD":/qmk_firmware --user $(id -u):$(id -g) qmkfm/base_container bin/qmk json2c bm40hsrgb_layout_planck_mit_mine.json | tail -n +2 >> ./keyboards/bm40hsrgb/keymaps/my/keymap.c
+
 ./util/docker_build.sh bm40hsrgb:my
