c220be9078a58d63f2e0ad0bd1749240343baea7
Red Forks <redforks@gmail.com>
2021-03-21 16:34:39 +0800

    Disable not used features

--- keyboards/ergodone/config.h
+++ keyboards/ergodone/config.h
@@ -82,8 +82,8 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
 //#define DEBUG_MATRIX_SCAN_RATE
 
 #endif
